f0687629952f2d6f85d6480a9e31648ad656f8ba
judgelord <devin.jl@gmail.com>
2026-02-19 09:42:18 -0500

    increase default line height to 1.3

--- code/copy syllabus template insert to other courses.R
+++ code/copy syllabus template insert to other courses.R
@@ -9,9 +9,21 @@ courses = c(
 )
 
 for (i in courses){
+  # copy syllabus insert
   file.copy(
     from = here::here("syllabus-insert.md"),
     to = here::here("..", i, "syllabus-insert.md")
+  )
+  # copy html style sheet
+  file.copy(
+    from = here::here("michigan.scss"),
+    to = here::here("..", i, "michigan.scss")
+  )
+  # copy accessibility fixes
+  dir.create(here::here("..", i, "assets"))
+  file.copy(
+    from = here::here("assets", "accessiblity-fixes.scss"),
+    to = here::here("..", i, "assets", "accessiblity-fixes.scss")
     )
 }
 
